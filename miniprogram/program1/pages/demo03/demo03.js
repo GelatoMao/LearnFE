878f904dbbba4a176719067fab350397686fc3d8
GelatoMao <gelatomao@outlook.com>
2021-01-06 23:28:40 +0800

    mina data binding

---
+++ miniprogram/program1/pages/demo03/demo03.js
@@ -0,0 +1,17 @@
+//Page Object
+Page({
+  // 页面中显示的数据都放在data当中
+  data: {
+    msg: 'hello mina',
+    num: 1000,
+    isExercise: true,
+    person: {
+      age: 18,
+      height: 145,
+      weight: 200,
+      name: 'haha'
+    },
+    isChecked: true
+  },
+});
+
